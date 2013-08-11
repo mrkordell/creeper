d173b4f2e3f3e33f4dde4e62393ca23af426ae51
Chris Serino <themindoverall@gmail.com>
2013-08-11 15:23:14 -0400

    Implement api_key generation.

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,4 +1,17 @@
+require 'securerandom'
+
 class Site < ActiveRecord::Base
 	belongs_to :owner, class_name: 'User'
 	has_many :visitors
+
+	before_create :generate_api_key!
+
+	def generate_api_key!
+		key = nil
+		begin
+			key = SecureRandom.hex
+		end while Site.where(api_key: key) > 0
+		self.api_key = key
+		self.save
+	end
 end
